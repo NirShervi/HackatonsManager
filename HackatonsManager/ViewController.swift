e5fec09a9cb915a9d17727cf3f9df118d8c39234
nirshrvi <nirshrvi@gmail.com>
2022-06-21 19:05:21 +0300

    Initial Commit

---
+++ HackatonsManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackatonsManager
+//
+//  Created by Nir Shervi on 21/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
